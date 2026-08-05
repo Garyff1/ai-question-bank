8359d393e74cd96a11f6c0d5bb65d3e94d68873e
Kevin <Garyff1@users.noreply.github.com>
2026-08-05 11:24:02 +0800

    ci: prefer official Android repositories before mirrors

--- android-app/android/build.gradle.kts
+++ android-app/android/build.gradle.kts
@@ -1,11 +1,11 @@
 allprojects {
     repositories {
+        google()
+        mavenCentral()
         maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { url = uri("https://maven.aliyun.com/repository/public") }
         maven { url = uri("https://maven.aliyun.com/repository/google") }
         maven { url = uri("https://maven.aliyun.com/repository/central") }
-        google()
-        mavenCentral()
     }
 }
 
